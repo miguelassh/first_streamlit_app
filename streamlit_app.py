5d884372f365da3accff179fbf987138016d28c0
miguelassh <62038439+miguelassh@users.noreply.github.com>
2024-02-21 12:57:00 +0100

    Update streamlit_app.py
    added the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,3 +26,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# create the pandas dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# shows the dataframe with streamlit
+streamlit.dataframe(fruityvice_normalized)
